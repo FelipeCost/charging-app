0e3eec551d4d1299078c52a47d7386bd47710eac
Felipe Costa <jfpcosta@Felipes-MacBook-Pro.local>
2026-01-05 05:31:34 +0000

    Fix S3 CSV read bug

--- s3_utils.py
+++ s3_utils.py
@@ -1,7 +1,9 @@
-import os
 import boto3
 import pandas as pd
-from io import StringIO
+import io
+import os
+from botocore.exceptions import ClientError
+
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
 AWS_REGION = os.environ.get("AWS_REGION", "eu-west-2")
@@ -11,22 +13,17 @@ s3 = boto3.client("s3", region_name=AWS_REGION)
 def read_csv_s3(key, columns=None):
     try:
         obj = s3.get_object(Bucket=S3_BUCKET, Key=key)
-        df = pd.read_csv(obj["Body"])
-        if columns:
-            for c in columns:
-                if c not in df.columns:
-                    df[c] = None
-        return df
-    except s3.exceptions.NoSuchKey:
-        if columns:
+        body = obj["Body"].read()
+        if len(body) == 0:
             return pd.DataFrame(columns=columns)
-        return pd.DataFrame()
-    except Exception:
-        if columns:
+        return pd.read_csv(io.BytesIO(body))
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
             return pd.DataFrame(columns=columns)
-        return pd.DataFrame()
+        raise
 
 def write_csv_s3(df, key):
-    csv_buffer = StringIO()
+    csv_buffer = io.StringIO()
     df.to_csv(csv_buffer, index=False)
     s3.put_object(Bucket=S3_BUCKET, Key=key, Body=csv_buffer.getvalue())
+
